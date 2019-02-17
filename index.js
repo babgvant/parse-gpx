c76be87f15794b93cf6589d91b469faa6699f0e7
babgvant <avantil@gmail.com>
2019-02-16 18:52:18 -0600

    Add XML data handling

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const xml2js = require('xml2js');
 const fs = require('fs');
 const parseTrack = require('./src/parseTrack');
 
-module.exports = (filename) => {
-
+function ParseFile(filename){
     return new Promise((res, rej) => {
         fs.readFile(filename,'utf8', (err, data) => {
             if(err) {
@@ -13,14 +12,23 @@ module.exports = (filename) => {
                 return;
             }
 
-            let parser = new xml2js.Parser();
-            parser.parseString(data, (err, xml) => {
-                if(err) {
-                    rej(err);
-                } else {
-                    res(parseTrack(xml.gpx.trk));
-                }
-            });
+            ParseXml(data);
+        });
+    });
+}
+
+function ParseXml(data){
+    return new Promise((res, rej) => {
+        let parser = new xml2js.Parser();
+        parser.parseString(data, (err, xml) => {
+            if(err) {
+                rej(err);
+            } else {
+                res(parseTrack(xml.gpx.trk));
+            }
         });
     });
 };
+
+module.exports.ParseFile = ParseFile;
+module.exports.ParseXml = ParseXml;
